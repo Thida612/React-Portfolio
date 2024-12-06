158318143f7e624e3ac719fde023930cd1c0819c
Thida612 <angelic722@gmail.com>
2024-12-06 17:16:19 -0600

    created about.jsx and Importing the React library to use JSX and create a functional component.

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+// Importing the React library to use JSX and create a functional component
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About me</h1>
+      <p>
+        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch
+        Etsy authentic Marfa quinoa typewriter plaid direct trade small batch
+        wayfarers...
+      </p>
+    </div>
+  );
+};
+
+// Exporting the About component to be used in other parts of the application
+export default About;
